98082ca1e13ff47c78314a107f32165208751d0d
Nikita Ostapuk <yaostin@proton.me>
2024-12-28 02:46:35 +0300

    asda

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <h1>Шашки</h1>
+      <Link href="/avatar/upload">Добавить аватар</Link>
+    </div>
+  );
 }
